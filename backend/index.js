fe595ac6206ce1e9ee208a7b0373c6a27e0ed0e6
Kassy-0703 <bump.kassy.0421@gmail.com>
2025-07-15 00:21:12 +0900

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const { Pool } = require('pg');
 const cors = require('cors'); // CORSミドルウェアをインポート
 
 const app = express();
